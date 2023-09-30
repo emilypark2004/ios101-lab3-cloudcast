55b70000575c2f1d3039e132141b81729934a1e2
emilypark2004 <emilykumap@gmail.com>
2023-09-29 21:28:23 -0700

    Initial Commit

---
+++ ForecastViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Emily Park on 9/28/23.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        selectedForecastIndex = max(0, selectedForecastIndex - 1)
+        configure(with: forecasts[selectedForecastIndex])
+    }
+    
+    @IBAction func didTapForwardButton(_ sender: UIButton) {
+        selectedForecastIndex = min(forecasts.count - 1,selectedForecastIndex + 1)
+        configure(with: forecasts[selectedForecastIndex])
+    }
+    
+    private var forecasts = [WeatherForecast]()
+    private var selectedForecastIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addGradient()
+        forecasts = createMockData()
+        configure(with: forecasts[selectedForecastIndex])
+    }
+
+    private func createMockData() -> [WeatherForecast] {
+        let today = Date()
+        var dateComponents = DateComponents()
+        dateComponents.day = 1
+        let tomorrow = Calendar.current.date(byAdding: dateComponents, to: today)!
+        let dayAfterTomorrow = Calendar.current.date(byAdding: dateComponents, to: tomorrow)!
+        let mockData1 = WeatherForecast(temperature: 59.5,
+                                            date:today,
+                                            weatherCode: .violentRainShowers
+                                            )
+        let mockData2 = WeatherForecast(temperature: 65.5,
+                                            date: tomorrow,
+                                            weatherCode: .fog
+                                            )
+        let mockData3 = WeatherForecast(temperature: 49.5,
+                                            date: dayAfterTomorrow,
+                                            weatherCode: .partlyCloudy
+                                            )
+        return [mockData1, mockData2, mockData3]
+    }
+        
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(red: 0.54, green: 0.88, blue: 0.99, alpha: 1.00).cgColor,
+                                UIColor(red: 0.51, green: 0.81, blue: 0.97, alpha: 1.00).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+        
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
+    }
+
+}
